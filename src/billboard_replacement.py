bfd4c8d08674a5b000abf4f67af85c5f325b23f4
TeamDman <TeamDman9201@gmail.com>
2020-10-19 17:05:14 -0400

    Disable contour logging

--- src/billboard_replacement.py
+++ src/billboard_replacement.py
@@ -62,8 +62,8 @@ def get_largest_billboard_contour(image):
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
         area = cv2.contourArea(contour)
-        print("Found contour with {} nodes and total area {}"
-              .format(len(approx), area))
+        # print("Found contour with {} nodes and total area {}"
+        #       .format(len(approx), area))
         # if len(approx) == 4:
         if area > best[0]:
             best = (area, approx)
@@ -109,11 +109,15 @@ def get_homo_warped_decal(background, decal, contour):
 
 #%%
 # noinspection PyShadowingNames
-def apply_image_to_billboard(source_image, labeled_source_image, decal_image, display_steps=False):
+def apply_image_to_billboard(source_image, labeled_source_image, decal_image):
+    # pad image so billboards touching the edge can be detected properly
     padded_source_image = add_borders(source_image)
     padded_labeled_source_image = add_borders(labeled_source_image)
+
+    # create image with the billboard being the only white spot
     binary_image = strip_colours(padded_labeled_source_image)
 
+    # identify the billboard border
     contour = get_largest_billboard_contour(binary_image)
     outlined_image = binary_image.copy()
     outlined_image = cv2.drawContours(
@@ -124,59 +128,49 @@ def apply_image_to_billboard(source_image, labeled_source_image, decal_image, di
         3
     )
 
-
+    # create mask for replacing billboard iamge
     blank_image = np.zeros(padded_source_image.shape, dtype=np.uint8)
     mask_image = get_contour_mask(blank_image, contour)
     mask_inv_image = cv2.bitwise_not(mask_image)
 
+    # remove the old billboard decal
     labeled_image_no_billboard = cv2.bitwise_and(padded_labeled_source_image, mask_inv_image)
     source_image_no_billboard = cv2.bitwise_and(padded_source_image, mask_inv_image)
 
-
+    # warp the decal to fit the bilboard
     warped_image = get_homo_warped_decal(outlined_image, decal_image, contour)
+
+    # apply the decal to the billboard
     output_image = cv2.bitwise_or(source_image_no_billboard, warped_image)
+
+    # remove border we applied earlier
     output_image_borderless = remove_borders(output_image)
 
-    if display_steps:
-        display(padded_source_image)
-        display(padded_labeled_source_image)
-        display(binary_image)
-        display(outlined_image)
-        circle_image = padded_labeled_source_image.copy()
-        colors = [(0,255,0),(255,0,0),(255,255,0),(0,255,255)]
-        for i, center in enumerate([x[0] for x in contour]):
-            cv2.circle(circle_image, tuple(center), 20, colors[i], -1)
-        display(circle_image)
-        display(mask_image)
-        display(mask_inv_image)
-        display(labeled_image_no_billboard)
-        display(padded_source_image)
-        display(source_image_no_billboard)
-        display(
+    # helper image to show contour points
+    circle_image = padded_labeled_source_image.copy()
+    colors = [(0,255,0),(255,0,0),(255,255,0),(0,255,255)]
+    for i, center in enumerate([x[0] for x in contour]):
+        cv2.circle(circle_image, tuple(center), 20, colors[i], -1)
+
+    return output_image_borderless, contour, [
+        padded_source_image,
+        padded_labeled_source_image,
+        binary_image,
+        outlined_image,
+        circle_image,
+        mask_image,
+        mask_inv_image,
+        labeled_image_no_billboard,
+        padded_source_image,
+        source_image_no_billboard,
+        cv2.resize(
             decal_image,
-            scale_x=padded_source_image.shape[1]/decal_image.shape[1]*0.5,
-            scale_y=padded_source_image.shape[0]/decal_image.shape[0]*0.5,
-        )
-        display(warped_image)
-        display(output_image)
-
-    return output_image_borderless
-
-#%%
-source_image = cv2.imread("../resources/new/015_00010.jpg")
-labeled_image = cv2.imread("../resources/new/015_00010.png")
-decal_image = cv2.imread("../resources/new/milk_sip.png")
-# decal_image = cv2.imread("../resources/new/decal_0.png")
-output_image = apply_image_to_billboard(
-    source_image,
-    labeled_image,
-    decal_image,
-    display_steps=False
-)
-
-display(output_image)
-
+            (0,0),
+            fx=padded_source_image.shape[1]/decal_image.shape[1]*0.5,
+            fy=padded_source_image.shape[0]/decal_image.shape[0]*0.5
+        ),
+        warped_image,
+        output_image,
+    ]
 
 #%%
-
-
