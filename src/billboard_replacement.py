74a9cccf2cc2ef53c1a86d385c4098cc6d94bb25
TeamDman <TeamDman9201@gmail.com>
2020-11-17 21:25:04 -0500

    Formatting

--- src/billboard_replacement.py
+++ src/billboard_replacement.py
@@ -1,32 +1,36 @@
-#%%
+# %%
 import cv2
 import numpy as np
 
-#%%
+# %%
 from common import playing_for_benchmarks_billboard_colour
 
 first = True
+
+
 def display(image, scale_x=0.5, scale_y=0.5):
     global first
-    shrunk = cv2.resize(image, (0,0), fx=scale_x, fy=scale_y)
+    shrunk = cv2.resize(image, (0, 0), fx=scale_x, fy=scale_y)
     cv2.imshow("Screen Location", shrunk)
 
     if cv2.waitKey(0 if first else 1000) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
     first = False
 
-#%%
+
+# %%
 
 def strip_colours(image, preserve=playing_for_benchmarks_billboard_colour):
     black_indices = np.where((image != preserve).all(axis=2))
     white_indices = np.where((image == preserve).all(axis=2))
     rtn = image.copy()
-    rtn[black_indices] = [0,0,0]
-    rtn[white_indices] = [255,255,255]
+    rtn[black_indices] = [0, 0, 0]
+    rtn[white_indices] = [255, 255, 255]
     return rtn
 
+
 # noinspection PyDefaultArgument
-def add_borders(image, border_color= [0,0,0], border_width=25):
+def add_borders(image, border_color=[0, 0, 0], border_width=25):
     return cv2.copyMakeBorder(
         image,
         border_width,
@@ -37,8 +41,10 @@ def add_borders(image, border_color= [0,0,0], border_width=25):
         value=border_color
     )
 
+
 def remove_borders(image, border_width=25):
     return image[border_width:-border_width, border_width:-border_width]
+
 
 # noinspection PyShadowingNames
 def get_largest_billboard_contour(image):
@@ -55,8 +61,7 @@ def get_largest_billboard_contour(image):
         reverse=True
     )[:10]
 
-
-    best = (0,0)
+    best = (0, 0)
 
     for contour in contours:
         peri = cv2.arcLength(contour, True)
@@ -70,6 +75,7 @@ def get_largest_billboard_contour(image):
 
     return best[1] if best[0] > 0 else None
 
+
 # noinspection PyShadowingNames
 def get_contour_mask(image, contour):
     rtn = image.copy()
@@ -77,13 +83,16 @@ def get_contour_mask(image, contour):
         image=rtn,
         contours=[contour],
         contourIdx=-1,
-        color=(255,255,255),
+        color=(255, 255, 255),
         thickness=cv2.FILLED
     )
     return rtn
 
+
 def rotate(lst, x):
     return lst[-x:] + lst[:-x]
+
+
 # noinspection PyShadowingNames
 def get_homo_warped_decal(background, decal, contour):
     contour = [[x[0]] for x in contour]
@@ -95,19 +104,20 @@ def get_homo_warped_decal(background, decal, contour):
     h_background, w_background = background.shape[:2]
     src = np.float32([
         [0, h_decal],
-        [0,0],
+        [0, 0],
         [w_decal, 0],
         [w_decal, h_decal],
     ])
     dst = np.float32(contour)
     h, mask = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
 
-    flipped = np.flip(decal,axis=1)
+    flipped = np.flip(decal, axis=1)
 
     warped = cv2.warpPerspective(flipped, h, (w_background, h_background))
     return warped
 
-#%%
+
+# %%
 # noinspection PyShadowingNames
 def apply_image_to_billboard(source_image, labeled_source_image, decal_image):
     # pad image so billboards touching the edge can be detected properly
@@ -124,7 +134,7 @@ def apply_image_to_billboard(source_image, labeled_source_image, decal_image):
         outlined_image,
         [contour],
         -1,
-        [0,255,0],
+        [0, 255, 0],
         3
     )
 
@@ -148,7 +158,7 @@ def apply_image_to_billboard(source_image, labeled_source_image, decal_image):
 
     # helper image to show contour points
     circle_image = padded_labeled_source_image.copy()
-    colors = [(0,255,0),(255,0,0),(255,255,0),(0,255,255)]
+    colors = [(0, 255, 0), (255, 0, 0), (255, 255, 0), (0, 255, 255)]
     for i, center in enumerate([x[0] for x in contour]):
         cv2.circle(circle_image, tuple(center), 20, colors[i], -1)
 
@@ -165,12 +175,12 @@ def apply_image_to_billboard(source_image, labeled_source_image, decal_image):
         source_image_no_billboard,
         cv2.resize(
             decal_image,
-            (0,0),
-            fx=padded_source_image.shape[1]/decal_image.shape[1]*0.5,
-            fy=padded_source_image.shape[0]/decal_image.shape[0]*0.5
+            (0, 0),
+            fx=padded_source_image.shape[1] / decal_image.shape[1] * 0.5,
+            fy=padded_source_image.shape[0] / decal_image.shape[0] * 0.5
         ),
         warped_image,
         output_image,
     ]
 
-#%%
+# %%
